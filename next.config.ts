4595bfa69047bcde1332c1d2401cda228019605e
heytechmate <heytechmate@gmail.com>
2026-01-27 15:02:17 +0300

    Force disable linting in package.json

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
+    // This still works to ignore Type errors
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  // We removed the 'eslint' block because it is now handled in package.json
 };
 
 export default nextConfig;
